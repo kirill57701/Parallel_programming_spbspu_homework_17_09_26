c8b4ba0a096b5c151d781606bfae85c74f86a817
Kirill <kirillpetrov56333@gmail.com>
2026-09-16 09:33:09 +0300

    adding clicker

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <chrono>
+
+class Clicker {
+public:
+  Clicker() : start_(std::chrono::high_resolution_clock::now()) {}
+
+  double millisec() const {
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+
+    auto t = high_resolution_clock::now();
+    return duration_cast<duration<double, std::milli>>(t - start_).count();
+  }
+
+private:
+  std::chrono::high_resolution_clock::time_point start_;
+};
 
 int main() {
   std::cout << "std::cout";
