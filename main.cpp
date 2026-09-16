6ea76fe15109a90dacaedc28eacbca2d06a070f1
Kirill <kirillpetrov56333@gmail.com>
2026-09-16 09:50:21 +0300

    init some params

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <future>
 
 class Clicker {
 public:
@@ -21,7 +22,8 @@ private:
 
 using data_t = std::vector< unsigned long long >;
 using value_t = data_t::value_type;
-int main() {
+int main(int argc, char** argv) {
+  int thrs = std::stoi(argv[1]);
   constexpr size_t size{1'000'000'000};
   double init{0}, total{0};
   value_t sum{0};
@@ -29,6 +31,11 @@ int main() {
     mtt::Clicker cl;
     data_t values(size, 1);
     init = cl.millisec();
+    std::vector<std::future<value_t>> fut;
+    fut.reserve(thrs);
+    int potok_s = size/thrs;
+    int ost = size % thrs;
+    int beg = 0;
     for (size_t i = 0; i < values.size(); ++i) {
     sum += values[i];
   }
